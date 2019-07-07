dc1055f44aceb79e81bb8cd532a4980bfaeddb80
Gabor Tanz <gabor.tanz@post.ch>
2019-07-07 19:02:08 +0200

    implement json encoding for most stuff

--- acl_json.go
+++ acl_json.go
@@ -18,8 +18,8 @@ func (a _abp) MarshalJSON() ([]byte, error) {
 }
 
 func (a ACLsByResource) MarshalJSON() ([]byte, error) {
-	length := len(a)
 	buffer := bytes.NewBufferString(`{`)
+	length := len(a)
 	i := 1
 	for r := range a {
 		buffer.WriteString(`"` + r.ResourceType + `_` + r.ResourceName + `":`)
@@ -33,6 +33,71 @@ func (a ACLsByResource) MarshalJSON() ([]byte, error) {
 		}
 		i++
 	}
+	buffer.WriteString(`}`)
+	return buffer.Bytes(), nil
+}
+
+func (a ACLsByPrincipalAndResource) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString(`{`)
+	principals := len(a)
+	i := 1
+	for p := range a {
+		buffer.WriteString(`"` + p + `":[`)
+		resources := len(a[p])
+		j := 1
+		for r := range a[p] {
+			buffer.WriteString(`{"` + r.ResourceType + `_` + r.ResourceName + `":`)
+			b, err := json.Marshal(a[p][r])
+			if err != nil {
+				return nil, err
+			}
+			buffer.Write(b)
+			buffer.WriteString(`}`)
+			if j != resources {
+				buffer.WriteString(`,`)
+			}
+			j++
+		}
+		buffer.WriteString(`]`)
+		if i != principals {
+			buffer.WriteString(`,`)
+		}
+		i++
+	}
+
+	buffer.WriteString(`}`)
+	return buffer.Bytes(), nil
+}
+
+func (a ACLsByResourceAndPrincipal) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString(`{`)
+	resources := len(a)
+	i := 1
+	for r := range a {
+		buffer.WriteString(`"` + r.ResourceType + `_` + r.ResourceName + `":[`)
+		principals := len(a[r])
+		j := 1
+		for p := range a[r] {
+			buffer.WriteString(`{"` + p + `":`)
+			b, err := json.Marshal(a[r][p])
+			if err != nil {
+				return nil, err
+			}
+			buffer.Write(b)
+			buffer.WriteString(`}`)
+			if j != principals {
+				buffer.WriteString(`,`)
+			}
+			j++
+		}
+		buffer.WriteString(`]`)
+		if i != resources {
+			buffer.WriteString(`,`)
+		}
+		i++
+	}
+
+
 	buffer.WriteString(`}`)
 	return buffer.Bytes(), nil
 }
--- acl_json_test.go
+++ acl_json_test.go
@@ -1,6 +1,7 @@
 package kafkalib
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 )
@@ -166,11 +167,164 @@ func TestACLsByResource_MarshalJSON(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := tt.a.MarshalJSON()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("_abr.MarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ACLsByPrincipalAndResource.MarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("_abr.MarshalJSON() = %s, want %s", got, tt.want)
+				t.Errorf("ACLsByPrincipalAndResource.MarshalJSON() = %s, want %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestACLsByPrincipalAndResource_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name    string
+		a       ACLsByPrincipalAndResource
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "three acls over two principals and 2 resources",
+			a: ACLsByPrincipalAndResource{
+				"User:test": map[Resource]ACLs{
+					Resource{
+						ResourceName: "test",
+						ResourceType: ResourceTopic,
+					}: {
+						ACL{
+							Principal:      "User:test",
+							PermissionType: PermissionAllow,
+							Operation:      OperationAlter,
+							Host:           "*",
+							Resource: Resource{
+								ResourceName: "test",
+								ResourceType: ResourceTopic,
+							},
+						},
+					},
+					Resource{
+						ResourceName: "test-2",
+						ResourceType: ResourceTopic,
+					}: {
+						ACL{
+							Principal:      "User:test",
+							PermissionType: PermissionAllow,
+							Operation:      OperationRead,
+							Host:           "*",
+							Resource: Resource{
+								ResourceName: "test-2",
+								ResourceType: ResourceTopic,
+							},
+						},
+					},
+				},
+				"User:two": map[Resource]ACLs{
+					Resource{
+						ResourceName: "test",
+						ResourceType: ResourceTopic,
+					}: {
+						ACL{
+							Principal:      "User:two",
+							PermissionType: PermissionAllow,
+							Operation:      OperationDescribe,
+							Host:           "*",
+							Resource: Resource{
+								ResourceName: "test",
+								ResourceType: ResourceTopic,
+							},
+						},
+					},
+				},
+			},
+			want:    []byte(`{"User:test":[{"Topic_test":[{"principal":"User:test","permission_type":"Allow","operation":"Alter","host":"*","resource_name":"test","resource_type":"Topic"}]},{"Topic_test-2":[{"principal":"User:test","permission_type":"Allow","operation":"Read","host":"*","resource_name":"test-2","resource_type":"Topic"}]}],"User:two":[{"Topic_test":[{"principal":"User:two","permission_type":"Allow","operation":"Describe","host":"*","resource_name":"test","resource_type":"Topic"}]}]}`),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.a.MarshalJSON()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ACLsByPrincipalAndResource.MarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !json.Valid(got) {
+				t.Errorf("ACLsByPrincipalAndResource.MarshalJSON() is invalid = %s, want like this %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestACLsByResourceAndPrincipal_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name    string
+		a       ACLsByResourceAndPrincipal
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "three acls over two principals and 2 resources",
+			a: ACLsByResourceAndPrincipal{
+				Resource{
+					ResourceName: "test",
+					ResourceType: ResourceTopic,
+				}: map[string]ACLs{
+					"User:test": {
+						ACL{
+							Principal:      "User:test",
+							PermissionType: PermissionAllow,
+							Operation:      OperationAlter,
+							Host:           "*",
+							Resource: Resource{
+								ResourceName: "test",
+								ResourceType: ResourceTopic,
+							},
+						},
+					},
+					"User:two": {
+						ACL{
+							Principal:      "User:two",
+							PermissionType: PermissionAllow,
+							Operation:      OperationDescribe,
+							Host:           "*",
+							Resource: Resource{
+								ResourceName: "test",
+								ResourceType: ResourceTopic,
+							},
+						},
+					},
+				},
+				Resource{
+					ResourceName: "test-2",
+					ResourceType: ResourceTopic,
+				}: map[string]ACLs{
+					"User:test": {
+						ACL{
+							Principal:      "User:test",
+							PermissionType: PermissionAllow,
+							Operation:      OperationRead,
+							Host:           "*",
+							Resource: Resource{
+								ResourceName: "test-2",
+								ResourceType: ResourceTopic,
+							},
+						},
+					},
+				},
+			},
+			want: []byte(`{"Topic_test":[{"User:test":[{"principal":"User:test","permission_type":"Allow","operation":"Alter","host":"*","resource_name":"test","resource_type":"Topic"}]},{"User:two":[{"principal":"User:two","permission_type":"Allow","operation":"Describe","host":"*","resource_name":"test","resource_type":"Topic"}]}],"Topic_test-2":[{"User:test":[{"principal":"User:test","permission_type":"Allow","operation":"Read","host":"*","resource_name":"test-2","resource_type":"Topic"}]}]}`),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.a.MarshalJSON()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ACLsByResourceAndPrincipal.MarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !json.Valid(got) {
+				t.Errorf("ACLsByResourceAndPrincipal.MarshalJSON() is invalid = %s, want like this %s", got, tt.want)
 			}
 		})
 	}
