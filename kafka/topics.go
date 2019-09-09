af621214b0c8902dc4a4ffe22035812a0d266c56
Gabor Tanz <gabor.tanz@post.ch>
2019-09-09 15:02:15 +0200

    add topic filtering

--- kafka/topics.go
+++ kafka/topics.go
@@ -14,34 +14,6 @@ type NewTopic struct {
 	sarama.TopicDetail
 }
 
-// GetTopic returns the topic defined in the Name of the client
-func (c Conn) GetTopic(filter string) (format.Topics, error) {
-	allTopics, err := c.AdminClient.ListTopics()
-	if err != nil {
-		return nil, fmt.Errorf("Error getting allTopics: %s", err)
-	}
-	topics := format.Topics{}
-	r, err := regexp.Compile(filter)
-	for k, v := range allTopics {
-		if r.MatchString(k) {
-			topics[k] = v
-		}
-	}
-	if len(topics) > 0 {
-		return topics, nil
-	}
-	return nil, fmt.Errorf("Topic %s not found", filter)
-}
-
-// GetAllTopics returns all known topics
-func (c Conn) GetAllTopics() (format.Topics, error) {
-	topics, err := c.AdminClient.ListTopics()
-	if err != nil {
-		return nil, fmt.Errorf("Error getting topics: %s", err)
-	}
-	return topics, nil
-}
-
 // CreateTopic creates the topic defined in the TopicClient
 func (c Conn) CreateTopic(topic NewTopic) error {
 	err := c.AdminClient.CreateTopic(topic.Name, &topic.TopicDetail, false)
--- topic_filter.go
+++ topic_filter.go
@@ -1,9 +1,12 @@
 package kafkalib
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
+// GetAllTopics returns all known topics
 func (c Conn) GetAllTopics() (Topics, error) {
-	var err error
 	topics, err := c.AdminClient.ListTopics()
 	if err != nil {
 		return nil, fmt.Errorf("Error getting topics: %s", err)
@@ -14,4 +17,28 @@ func (c Conn) GetAllTopics() (Topics, error) {
 		return nil, fmt.Errorf("Error converting topics: %s", err)
 	}
 	return t, nil
+}
+
+// GetTopic returns the topic defined in the Name of the client
+
+func (c Conn) GetTopic(filter string) (Topics, error) {
+	all, err := c.AdminClient.ListTopics()
+	if err != nil {
+		return nil, fmt.Errorf("Error getting topics: %s", err)
+	}
+	t := Topics{}
+	r, err := regexp.Compile(filter)
+	for k, v := range all {
+		if r.MatchString(k) {
+			t = append(t, Topic{
+				Name: k,
+				Partitions: int(v.NumPartitions),
+				ReplicationFactor: int(v.ReplicationFactor),
+			})
+		}
+	}
+	if len(t) > 0 {
+		return t, nil
+	}
+	return nil, fmt.Errorf("Topic %s not found", filter)
 }
