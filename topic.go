9889097f54162a1d3ac5d0487e5a8a03954ce34e
Gabor Tanz <gabor.tanz@post.ch>
2019-09-09 16:15:47 +0200

    use format text for multiple topics

--- topic.go
+++ topic.go
@@ -17,25 +17,26 @@ type Topic struct {
 
 type Topics []Topic
 
-func (t Topic) MarshalText() ([]byte, error) {
+func (t Topics) MarshalText() ([]byte, error) {
 	buf := bytes.Buffer{}
 	w := tabwriter.NewWriter(&buf, 2, 8, 1, '\t', 0)
 	_, err := w.Write([]byte(`Topic\tPartitions\tReplicationFactor\tRetention\tACLs\n`))
 	if err != nil {
 		return nil, err
 	}
-	_, err = fmt.Fprintf(w, "%s\t%d\t%d\t%d\t\n",
-		t.Name, t.Partitions, t.ReplicationFactor, t.RetentionMs)
-	if err != nil {
-		return nil, err
-	}
-	for _, acl := range t.ACLs {
-		_, err = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", "", "", "", "", acl)
+	for _, topic := range t {
+		_, err = fmt.Fprintf(w, "%s\t%d\t%d\t%d\t\n",
+			topic.Name, topic.Partitions, topic.ReplicationFactor, topic.RetentionMs)
 		if err != nil {
 			return nil, err
 		}
+		for _, acl := range topic.ACLs {
+			_, err = fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n", "", "", "", "", acl)
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
-
 	err = w.Flush()
 	if err != nil {
 		return nil, err
